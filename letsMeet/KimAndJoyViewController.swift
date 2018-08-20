160c302dde8ce6ba97282bbc51c7db525e8bf52a
Jigyasaa Sood <jsood@ucsd.edu>
2018-08-19 21:49:55 -0700

    JJ commit

---
+++ letsMeet/KimAndJoyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  KimAndJoyViewController.swift
+//  letsMeet
+//
+//  Created by jsood on 8/19/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+import UIKit
+
+class KimAndJoyViewController: UIViewController {
+    
+    
+    @IBOutlet weak var auhyisufyisuyfdLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
