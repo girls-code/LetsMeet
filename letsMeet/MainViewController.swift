b0bd8611823db909d199e60fa7fda30e359fa09c
Joy_Wang <joywang@Joy-Wangs-MacBook-Pro.local>
2018-08-28 21:20:15 -0700

    editing story board

---
+++ letsMeet/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  letsMeet
+//
+//  Created by Joy_Wang on 8/28/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var MapImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
